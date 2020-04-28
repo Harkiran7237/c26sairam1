dc48419b0c637396c645304412bf19f11534b694
harkiran7237 <harkiran7237@gmail.com>
2020-04-28 17:20:12 +0530

    Changed pig

--- Pig.js
+++ Pig.js
@@ -2,6 +2,17 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.Visibility=155;
   }
-
+display(){
+  console.log(this.body.speed);
+  if(this.body.speed<3){
+    super.display();
+  }
+  else{
+    World.remove(world,this.body);
+  }
+  //hello
+  World.add;
+}
 };
